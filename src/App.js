73b1a0d31fb4a5a37887828191c78add7d84976b
DaanBrouwer <daanbrouwer96@gmail.com>
2022-03-15 10:16:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import React, { Component } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import About from "./components/About";
-import ErrorPage from "./components/ErrorPage";
-import {Nav, Navbar, Container} from 'bootstrap/dist/css/bootstrap.css'
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import About from "./Pages/About";
+import ErrorPage from "./Pages/ErrorPage";
+import { nav, container, navbar } from "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="app">
-      
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Ontdekstation013</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-        <Link to="/login"> Login</Link>
-        </li>
-        <li class="nav-item">
-        <Link to="/"> Home</Link>
-        </li>
-        <li class="nav-item">
-        <Link to="/about"> About</Link>
-        </li>      
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Ontdekstation013
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/login"> Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/"> Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about"> About</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
       {/* <nav>
         <Link to="/"> Home</Link>
