39e167638d3ac5187a406271b666128e011da4f1
Jaeden1503 <fabienneleidekker@gmail.com>
2022-03-29 10:47:00 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { Component, useState } from "react";
+import { Routes, Route, Link, useResolvedPath } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import About from "./Pages/About";
@@ -7,11 +7,43 @@ import ErrorPage from "./Pages/ErrorPage";
 import { nav, container, navbar } from "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+
+  const adminUser = {
+    name: "admin@gmail.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: ""});
+  const [error, setError] = useState("");
+
+  const LoggingIn = details => {
+    console.log(details);
+
+    if (details.name == adminUser.name && details.password == adminUser.password)
+    {
+      console.log("Ingelogd");
+      setUser({
+        name: details.name
+      });
+    }
+    else
+    {
+      console.log("onjuiste gegevens ingevuld");
+      setError("onjuiste gegevens ingevuld");
+    } 
+
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: ""});
+  }
+
   return (
     <div className="app">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             Ontdekstation013
           </a>
           <button
@@ -28,18 +60,26 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link to="/login"> Login</Link>
+                <Link className="nav-link" to="/login"> Login</Link>
               </li>
               <li className="nav-item">
-                <Link to="/"> Home</Link>
+                <Link className="nav-link" to="/"> Home</Link>
               </li>
               <li className="nav-item">
-                <Link to="/about"> About</Link>
+                <Link className="nav-link" to="/about"> About</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
+
+      {(user.name != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+          </div>
+      ) : (<Login Login = {LoggingIn} error = {error}/>
+      )}
 
       {/* <nav>
         <Link to="/"> Home</Link>
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Login() {
+export default function Login({Login, error}) {
+  const [details, setDetails] = useState({name: "", password: ""});
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    Login(details);
+  }
+
   return (
-<div> this is the login component
-    <h1>Login</h1>
-
+    <div className='container'>
+      <div className="d-flex justify-content-center h-100 card-margin">
+        <div className="card">
+          <div className="card-header">
+            <h3>Inloggen</h3>
+          </div>
+          <div className="card-body">          
+            <form onSubmit={submitHandler}>
+                <div>{(error != "") ? (<div className='error'>{error}</div>) : ""}</div>
+              <div className="input-group form-group login-boxes-margin">
+                <input type="email" className="form-control" placeholder="gebruikersnaam" onChange={e => setDetails({...details, name: e.target.value})} value= {details.name} />
+              </div>
+              <div className="input-group form-group login-boxes-margin">
+                <input type="password" className="form-control" placeholder="wachtwoord" onChange={e => setDetails({...details, password: e.target.value})} value= {details.password}/>
+              </div>
+              <div className="form-group center login-button-margin">
+                <input type="submit" value="Login" className="btn btn-info login_btn" />
+              </div>
+            </form>        
+          </div>
+          <div className="card-footer">
+            <div className="d-flex justify-content-center links">
+              Nog geen account?<a href="#">Registreer</a>
+            </div>
+            <div className="d-flex justify-content-center">
+              <a href="#">Wachtwoord vergeten?</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
