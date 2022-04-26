11db02c86ae81baa483322a0ebb3b141d86c3560
Jaeden1503 <fabienneleidekker@gmail.com>
2022-04-26 09:51:28 +0200

    updated test

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -2,45 +2,42 @@
 
 describe("Log in page", () => {
         
-    it("testing input", () => {
+    it("should login", () => {
         
         const User = {
             email: "admin@gmail.com",
             password: "admin"
         }
 
+         cy.fixture('user_mock_data').then(function(data)
+        {
+            this.data=data;
+        })
+
+        cy.intercept("POST", "http://localhost:8082/api/Authentication/login", {}).as("tryLogin");
+        cy.visit("/login");
+
+        cy.get('#email').type(User.email);
+        cy.get('#password').type(User.password);
+
+        cy.get('button').click();
+
+        // cy.wait('@tryLogin').then(({response}) => {
+        //     expect(response.statusCode).to.eq(500)
+
+        cy.wait('@tryLogin').then((interception) => {
+            const expectedValue = {
+                email: this.data.email,
+                password: this.data.password
+            };
+            expect(JSON.stringify(interception.request.body)).equal(
+                JSON.stringify(expectedValue)
+            );
+        });
 
 
-        cy.intercept('POST', '/login', {}).as('login');
-
-        cy.wait('@login').then(xhr => {
-            expect(xhr.response.statusCode).to.equal(404);
-          });
-
-
-
-        // cy.fixture('user_mock_data').then(function(data)
-        // {
-        //     this.data=data ;
-        // })
-       
-        //cy.intercept('GET', '**/login', { fixture: 'user_mock_data' }).as('getUser');
-        // cy.intercept('POST', '/login', {}).as('tryLogin');
-
-        // cy.visit("login");
-        // cy.get('#email').type(User.email);
-        // cy.get('#password').type(User.password);
-        
-        // cy.get('button').click();
-        // cy.wait('@tryLogin').then((interception) => {
-        //     const expectedValue = {
-        //         //fixture value
-        //         email: User.email,
-        //         password: User.password                
-        //     };
-        //     expect(JSON.stringify(interception.request.body)).equal(JSON.stringify(expectedValue));
-        // });                     //data from post request
-
-        // //cy.request('/login').its('status').should('include', '400')
+        //   cy.wait('@tryLogin').then(xhr => {
+        //     expect(xhr.response.statusCode).to.equal(404);
+        //   });
     });
 });
