3101da547ae79bb2cb2cce662d56b4172f990cb3
janiek <janiek2011@hotmail.com>
2022-05-17 12:53:39 +0200

    Home test link deleted
    Home test link deleted

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
 
 export default function Home() {
   return (
     <div> This is the Home component
     <h1>Home</h1>
-    <Link
-  to={{
-    pathname: '/Edit/' + 1,
-  }}
->link</Link>
     </div>
   )
 }
