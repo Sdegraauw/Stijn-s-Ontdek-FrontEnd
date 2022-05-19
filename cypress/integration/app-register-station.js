6138b8ccbc8691a1750cf29366c58cab96fb268d
Cyrion <cyrion.vandongen@student.fontys.nl>
2022-05-19 11:47:01 +0200

    Edit tests register station
    Add checks for the privacy checkbox

--- cypress/integration/app-register-station.js
+++ cypress/integration/app-register-station.js
@@ -1,6 +1,45 @@
 import RegisterStation from "../../src/Components/RegisterStation";
 
-it("Should POST the correct json", () => {
+it("Should POST the correct json WITHOUT public check", () => {
+  const userId = 1;
+  const Stationname = "Piet";
+  const Address = "Teststraat 123";
+  const Height = "20";
+  const Longitude = "20.222";
+  const Latitude = "20.1234";
+  const IsPublic = false;
+
+  cy.intercept("POST", "http://localhost:8082/api/Station", {}).as(
+    "addStation"
+  );
+  cy.visit("/register");
+
+  cy.get("[placeholder=Name]").type(Stationname);
+  cy.get("[placeholder=Adress]").type(Address);
+  cy.get(":nth-child(5) > .form-control").type(Height);
+  cy.get(":nth-child(6) > .form-control").type(Longitude);
+  cy.get(":nth-child(7) > .form-control").type(Latitude);
+
+  cy.get(".btn").click();
+
+  cy.wait("@addStation").then((interception) => {
+    const expectedValue = {
+      userId: userId, 
+      stationname: Stationname,
+      address: Address,
+      height: Height,
+      longitude: Longitude,
+      latitude: Latitude,
+      ispublic: IsPublic,
+    };
+    expect(JSON.stringify(interception.request.body)).equal(
+      JSON.stringify(expectedValue)
+    );
+  });
+});
+
+it("Should POST the correct json WITH public check", () => {
+  const userId = 1;
   const Stationname = "Piet";
   const Address = "Teststraat 123";
   const Height = "20";
@@ -18,17 +57,19 @@ it("Should POST the correct json", () => {
   cy.get(":nth-child(5) > .form-control").type(Height);
   cy.get(":nth-child(6) > .form-control").type(Longitude);
   cy.get(":nth-child(7) > .form-control").type(Latitude);
+  cy.get(':nth-child(8) > input').click();
 
   cy.get(".btn").click();
 
   cy.wait("@addStation").then((interception) => {
     const expectedValue = {
+      userId: userId, 
       stationname: Stationname,
       address: Address,
       height: Height,
       longitude: Longitude,
       latitude: Latitude,
-      IsPublic: IsPublic,
+      ispublic: IsPublic,
     };
     expect(JSON.stringify(interception.request.body)).equal(
       JSON.stringify(expectedValue)
