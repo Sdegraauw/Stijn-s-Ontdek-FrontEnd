02d9b35c4185f1cddda961482faca6738ba5f47a
Xander van Veen <493379@student.fontys.nl>
2022-11-28 15:50:35 +0100

    Setup navbar for responsive features

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,23 +4,21 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <header className="navbar-section">
-      <nav className="navbar navbar-expand-sm justify-content-between">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarLinks"
-          aria-controls="navbarLinks"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+      <nav className="navbar navbar-expand-lg justify-content-between">
+        {/* Brand */}
+        <a className="navbar-brand" href="#">Ontdekstation</a>
+
+        {/* Collapse button */}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+          aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand" href="/">
-          Ontdekstation013
-        </a>
-        <div className="navbar-nav justify-content-end" id="navbarLinks">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        {/* Collapsible content */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbar">
+          {/* Container for links */}
+          <ul className="navbar-nav mr-auto">
+            {/* Links */}
             <li className="nav-item">
               <Link className="nav-link" to="/about">Over ons</Link>
             </li>
