89e3c43a12c9999d25e5ab7cbb8dbe4893117825
Rob Boorsma <robboorsma@gmail.com>
2022-12-08 15:36:40 +0100

    Front end JWS Token
    Added functionality:
    
    When pressing the verify link, you will get 2 cookies and you will be "logged in"

--- src/Components/Verify.js
+++ src/Components/Verify.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from '../api/axios';
+import {useCookies} from "react-cookie";
 
 
 const url = '/Authentication/verify';
@@ -8,12 +9,16 @@ const url = '/Authentication/verify';
 const Verify = () => {
     const { linkHash, email } = useParams();
     const [error, setError] = useState(null);
-
+   const [cookies, setCookies, removeCookies] = useCookies();
     useEffect(() => {
         const getData = async () => {
             try {
                 const response = await axios.get(url , { params : {linkHash, email} });
                 console.log(response);
+                let expireTime = new Date();
+                expireTime.setDate(expireTime.getDate() + 7);
+                setCookies("firstName", response.data.firstName, {path: "/", expires: expireTime});
+                setCookies("JWT", response.data.jwsString, {path: "/", expires: expireTime});
                 setError(null);
             } catch (err) {
                 setError(err.message);
@@ -31,6 +36,7 @@ const Verify = () => {
             <p>email: {email}</p>
             <br/>
             <p>back end link: http://localhost:8080/api/Authentication/verify?linkHash={linkHash}&email={email}</p>
+            <p>Hi {cookies.firstName}</p>
         </div>
     );
 }
