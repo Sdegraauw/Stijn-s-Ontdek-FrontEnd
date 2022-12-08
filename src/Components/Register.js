8d443073ba9f59512d08a35344b09a232a050a4d
Xander van Veen <493379@student.fontys.nl>
2022-12-08 15:07:50 +0100

    Changed the way errors get rendered on the page

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -104,8 +104,16 @@ const Register = () => {
 
 return (
   <section className="form-section">
-      <p ref={errRef} className={errMsg ? "error-msg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-      <p ref={successRef} className={successMsg ? "success-msg" : "offscreen"} aria-live="assertive">{successMsg}</p>
+      {
+        errMsg && (
+          <div ref={errRef} className="error-msg">{errMsg}</div>
+        )
+      }
+      {
+        successMsg && (
+          <div ref={successRef} className="success-msg">{successMsg}</div>
+        )
+      }
       <h1>Registreren</h1>
 
       <form onSubmit={handleSubmit}>
