ebc6074e827389241607260e76c10f5e46495338
bas <schipper.b@outlook.com>
2023-11-13 15:51:17 +0100

    recentered the map

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -94,7 +94,7 @@ const Home = () => {
                         </div>
                     )
                 }
-                <MapContainer center={[51.565120, 5.066322]} zoom={13}>
+                <MapContainer center={[51.575120, 5.066322]} zoom={13}>
                     <TileLayer 
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
