9cbfbaaec92cfbd7cebc7337470758cd2a7653af
Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
2023-11-20 16:25:16 +0100

    abstraction fix
    -if statement excludes fields instead of including them

--- src/Components/FieldNameRadioButton.js
+++ src/Components/FieldNameRadioButton.js
@@ -9,13 +9,9 @@ const Radiobutton = ({data, handleChange, current}) =>{
 
     let fieldNames = Object.keys(data[0]);
 
-    for (let i = 0; i < fieldNames.length; i++){
-        fieldNames[i] = fieldNames[i].charAt(0).toUpperCase() + fieldNames[i].substring(1);
-    }
-
     const results = [];
     fieldNames.forEach(fieldName => {
-        if (fieldName === 'Temperature' || fieldName === 'Humidity') {
+        if (fieldName !== 'id' && fieldName !== 'latitude' && fieldName !== 'longitude' && fieldName !== 'timestamp') {
             results.push(
                 <div>
                     <label>
@@ -24,7 +20,7 @@ const Radiobutton = ({data, handleChange, current}) =>{
                                    handleChange(fieldName.toLowerCase());
                                    setSelectedOption(fieldName);
                                }}/>
-                            { fieldName }
+                            { fieldName.charAt(0).toUpperCase() + fieldName.substring(1) }
                     </label>
                     <br/>
                 </div>
