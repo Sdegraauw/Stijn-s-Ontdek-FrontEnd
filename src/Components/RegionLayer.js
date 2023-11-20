2b55c4e4d6c3203e6303a4bd241c33fdd7b8178c
Lars-v-Delft <lars171998@gmail.com>
2023-11-20 16:09:29 +0100

    changed color pallete with preset values

--- src/Components/RegionLayer.js
+++ src/Components/RegionLayer.js
@@ -25,38 +25,32 @@ const RegionLayer = ({ data, toggleRegion }) => {
         if (maxtemp - mintemp !== 0) {
             tempDif = maxtemp - mintemp;
         }
+        console.log("mintemp" + mintemp);
+        console.log("maxtemp" + maxtemp);
+        console.log("tempDif" + tempDif);
+    }
+
+    var colorDictionary = {
+        0: "rgb(206,255,255)",
+        1: "rgb(198,247,214)",
+        2: "rgb(162,244,155)",
+        3: "rgb(187,228,83)",
+        4: "rgb(213,206,4)",
+        5: "rgb(231,181,3)",
+        6: "rgb(241,153,3)",
+        7: "rgb(246,121,11)",
+        8: "rgb(249,73,2)",
+        9: "rgb(228,5,21)",
+        10: "rgb(168,0,3)",
     }
 
     function setRegionColour(value) {
         if (isNaN(value))
-            return "rgb(100,100,100)";
+            return "rgb(136,136,136)";
 
         let contrastValue = (value - mintemp) / tempDif;
-
-        if (contrastValue < 0)
-            contrastValue = 0;
-        else if (contrastValue > 1)
-            contrastValue = 1;
-
-        let red = Math.round(Red(contrastValue) * 255);
-        let green = Math.round(Green(contrastValue) * 255);
-        let blue = Math.round(Blue(contrastValue) * 255);
-
-        return "rgb(" + red.toString() + "," +
-            green.toString() + "," +
-            blue.toString() + ")";
-    }
-
-    function Red(contrastValue) {
-        return (Math.pow(2, contrastValue) - 1);
-    }
-
-    function Green(contrastValue) {
-        return Math.abs((-4 * Math.pow(contrastValue, 2)) + (4 * contrastValue));
-    }
-
-    function Blue(contrastValue) {
-        return ((Math.pow(2, 1 - contrastValue) - 1));
+        let colorIndex = Math.round(contrastValue * (Object.keys(colorDictionary).length - 1));
+        return colorDictionary[colorIndex];;
     }
 
     return (
@@ -64,7 +58,7 @@ const RegionLayer = ({ data, toggleRegion }) => {
             {
                 data.map((neighbourhood) => (
 
-                    <Polygon positions={neighbourhood.coordinates } key={neighbourhood.id} pathOptions={{color:setRegionColour(neighbourhood.avgTemp)}} opacity={ neighbourhood.avgTemp === "NaN" ? .4 : 1 } fillOpacity={ neighbourhood.avgTemp === "NaN" ? .25 : .5 }>
+                    <Polygon positions={neighbourhood.coordinates} key={neighbourhood.id} pathOptions={{ color: setRegionColour(neighbourhood.avgTemp) }} opacity={neighbourhood.avgTemp === "NaN" ? .4 : 1} fillOpacity={neighbourhood.avgTemp === "NaN" ? .25 : .5}>
                         <Popup>
                             <label className="bold">{neighbourhood.name}</label> <br />
 
