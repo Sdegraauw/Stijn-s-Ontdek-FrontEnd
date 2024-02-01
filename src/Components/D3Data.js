c83f7c7c283d8a587a63de2b3491aedb0e9d6abe
Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
2024-02-01 17:21:28 +0100

    aaaaaaaaaaaa

--- src/Components/D3Data.js
+++ src/Components/D3Data.js
@@ -5,11 +5,25 @@ const Chart = (Data) =>{
     let height;
 
     let D2Arr=[];
-    
+    let arrwidth=40;
+    let arrheight=40;
+    data.measurements.forEach(measurement=>{
+        if (measurement.longitude > arrwidth){
+            arrwidth = measurement.longitude;
+        }
+        if (measurement.latitude > arrheight){
+            arrheight = measurement.latitude;
+        }
+    })
+    for (let w = 0; w < arrwidth; w++){
+        for (let h = 0; h < arrheight; h++){
+        }
+    }
+
     const contours = d3.contours()
     contours
         .size([width, height])
         .thresholds([0, 1, 2, 3, 4]);
-    const polygons = contours()
+    const polygons = contours(D2Arr)
 }
 export default Chart;
